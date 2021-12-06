b7b00bd2ba1cbb7d5e2c9bf165aea999347d2392
futchitwo <74236683+futchitwo@users.noreply.github.com>
2021-12-07 06:34:49 +0900

    Add support for gigalixir

--- packages/backend/src/config/load.ts
+++ packages/backend/src/config/load.ts
@@ -26,7 +26,26 @@ const path = process.env.NODE_ENV === 'test'
 
 export default function load() {
 	const meta = JSON.parse(fs.readFileSync(`${_dirname}/../../../../built/meta.json`, 'utf-8'));
-	const config = yaml.load(fs.readFileSync(path, 'utf-8')) as Source;
+	const config = {
+		url: process.env.CONFIG_URL,
+		port:process.env.PORT,
+		db: {
+			host: process.env.DATABASE_HOST,
+			db: process.env.DATABASE_DB,
+			user: process.env.DATABASE_USER,
+			pass: process.env.DATABASE_PASS,
+			disableCache: true
+			//, extra: { max: process.env.DATABASE_MAX }
+		},
+		redis: {
+			host: process.env.REDIS_HOST,
+			port: process.env.REDIS_PORT,
+			pass: process.env.REDIS_PASS
+		},
+		id: "aid",
+		disableHsts: true,
+		clusterLimit: 1
+	}; //yaml.load(fs.readFileSync(path, 'utf-8')) as Source;
 
 	const mixin = {} as Mixin;
 
