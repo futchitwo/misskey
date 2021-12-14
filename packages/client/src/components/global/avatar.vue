571d8147c461e8d0e56188167cf9ab97eb2148b9
futchitwo <74236683+futchitwo@users.noreply.github.com>
2021-12-15 06:45:40 +0900

    Feature: birthday crown

--- packages/client/src/components/global/avatar.vue
+++ packages/client/src/components/global/avatar.vue
@@ -1,5 +1,8 @@
 <template>
 <span v-if="disableLink" v-user-preview="disablePreview ? undefined : user.id" class="eiwwqkts _noSelect" :class="{ cat, square: $store.state.squareAvatars }" :title="acct(user)" @click="onClick">
+	<svg v-if="birthday" class="crown" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
+		<path fill="#ffcc4d" d="M 24,15 C 20,8 20,8 18,4 l -6,11 c -1,1 0.831563,0.370231 0.147987,-0.02962 C 8.9310345,13.08867 4,8 4,8 4,8 6,15.75 6,28 h 24 c 0,-12.25 2,-20 2,-20 0,0 -5,5 -8,7 z"></path><circle fill="#5c913b" cx="22" cy="18" r="1.8"></circle><circle fill="#dd2e44" cx="30" cy="20" r="1.8"></circle><circle fill="#981ceb" cx="14" cy="18" r="1.8"></circle><circle fill="#dd2e44" cx="6" cy="20" r="1.8"></circle><circle fill="#ffbb4d" cx="4" cy="8" r="1.407"></circle><circle fill="#ffbb4d" cx="18" cy="4" r="1.784"></circle><circle fill="#ffbb4d" cx="32" cy="8" r="1.407"></circle>
+	</svg>
 	<img class="inner" :src="url" decoding="async"/>
 	<MkUserOnlineIndicator v-if="showIndicator" class="indicator" :user="user"/>
 </span>
@@ -46,6 +49,12 @@ export default defineComponent({
 	computed: {
 		cat(): boolean {
 			return this.user.isCat;
+		},
+		birthday(): boolean {
+			if (this.user.birthday == null) return;
+			const birthDate = new Date(this.user.birthday+"T00:00:00"); // local time zone
+			const today = new Date();
+			return (birthDate.getDate() === today.getDate()) && (birthDate.getMonth() === today.getMonth());
 		},
 		url(): string {
 			return this.$store.state.disableShowingAnimatedImages
@@ -97,6 +106,14 @@ export default defineComponent({
 	border-radius: 100%;
 	line-height: 16px;
 
+	> .crown {
+		position: absolute;
+		width: 65%;
+		top: -35%;
+		left: -5%;
+		transform: rotate(339deg);
+	}
+
 	> .inner {
 		position: absolute;
 		bottom: 0;
@@ -129,6 +146,12 @@ export default defineComponent({
 	}
 
 	&.cat {
+		> .crown {
+			transform: none;
+			margin-left: 30%;
+			width: 50%;
+		}
+
 		&:before, &:after {
 			background: #df548f;
 			border: solid 4px currentColor;
