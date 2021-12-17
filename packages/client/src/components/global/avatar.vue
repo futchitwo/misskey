2d28bf6080ff67316954912cad09bd7aa974a4a4
futchitwo <74236683+futchitwo@users.noreply.github.com>
2021-12-17 21:41:49 +0900

    Fix: avatar crown with link

--- packages/client/src/components/global/avatar.vue
+++ packages/client/src/components/global/avatar.vue
@@ -7,6 +7,9 @@
 	<MkUserOnlineIndicator v-if="showIndicator" class="indicator" :user="user"/>
 </span>
 <MkA v-else v-user-preview="disablePreview ? undefined : user.id" class="eiwwqkts _noSelect" :class="{ cat, square: $store.state.squareAvatars }" :to="userPage(user)" :title="acct(user)" :target="target">
+	<svg v-if="birthday" class="crown" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
+		<path fill="#ffcc4d" d="M 24,15 C 20,8 20,8 18,4 l -6,11 c -1,1 0.831563,0.370231 0.147987,-0.02962 C 8.9310345,13.08867 4,8 4,8 4,8 6,15.75 6,28 h 24 c 0,-12.25 2,-20 2,-20 0,0 -5,5 -8,7 z"></path><circle fill="#5c913b" cx="22" cy="18" r="1.8"></circle><circle fill="#dd2e44" cx="30" cy="20" r="1.8"></circle><circle fill="#981ceb" cx="14" cy="18" r="1.8"></circle><circle fill="#dd2e44" cx="6" cy="20" r="1.8"></circle><circle fill="#ffbb4d" cx="4" cy="8" r="1.407"></circle><circle fill="#ffbb4d" cx="18" cy="4" r="1.784"></circle><circle fill="#ffbb4d" cx="32" cy="8" r="1.407"></circle>
+	</svg>
 	<img class="inner" :src="url" decoding="async"/>
 	<MkUserOnlineIndicator v-if="showIndicator" class="indicator" :user="user"/>
 </MkA>
