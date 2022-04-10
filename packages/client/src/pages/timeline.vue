e183abda2eada51147796ae5fe6aedb2b3678ca0
futchitwo <74236683+futchitwo@users.noreply.github.com>
2022-04-11 07:55:43 +0900

    Update timeline.vue

--- packages/client/src/pages/timeline.vue
+++ packages/client/src/pages/timeline.vue
@@ -6,7 +6,7 @@
 
 		<div v-if="queue > 0" class="new"><button class="_buttonPrimary" @click="top()">{{ $ts.newNoteRecived }}</button></div>
 		<div class="tl _block">
-			<XTimeline ref="tl" :key="src"
+			<XTimeline ref="tlComponent" :key="src"
 				class="tl"
 				:src="src"
 				:sound="true"
