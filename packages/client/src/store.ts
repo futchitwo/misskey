acd0743974d01e18ebba1eab0e8b405882baf820
futchitwo <futchitwo@yahoo.co.jp>
2022-06-23 14:43:01 +0000

    disable blur by default

--- packages/client/src/store.ts
+++ packages/client/src/store.ts
@@ -149,11 +149,11 @@ export const defaultStore = markRaw(new Storage('base', {
 	},
 	useBlurEffectForModal: {
 		where: 'device',
-		default: true
+		default: false
 	},
 	useBlurEffect: {
 		where: 'device',
-		default: true
+		default: false
 	},
 	showFixedPostForm: {
 		where: 'device',
